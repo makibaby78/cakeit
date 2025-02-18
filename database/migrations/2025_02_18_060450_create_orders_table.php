62d64fcece69661f89abad017cd2f387d20fc747
Max Loued Baisac <raindrops.dark@gmail.com>
2025-02-18 15:00:07 +0800

    migration and model setup

---
+++ database/migrations/2025_02_18_060450_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number')->unique();
+            $table->string('name');
+            $table->string('contact_number');
+            $table->string('address');
+            $table->string('message');
+            $table->datetime('delivery_time');
+            $table->string('delivery_fee');
+            $table->decimal('payment', 10, 2);
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
